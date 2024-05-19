b4d5c34e32b65f5b0ef7b830db3ce5fc676fe0bf
Solanito97 <bjsolano97@gmail.com>
2024-05-19 15:50:05 -0600

    add edit in curso and profesor

--- Curso/scriptCurso.js
+++ Curso/scriptCurso.js
@@ -4,11 +4,13 @@ let formulario = document.getElementById('formulario');
 let nombrePagina = document.title;
 let nombreModuloListar = 'Lista Curso';
 let nombreModuloCrear = 'Crear Curso';
+let formularioEditar = document.getElementById('formularioEditar');
 
 
 let url= "https://paginas-web-cr.com/Api/apis/";
 let listar = "ListaCurso.php";
 let insertar = "InsertarCursos.php";
+let actualizar = "ActualizarCursos.php";
 
 
 
@@ -16,7 +18,6 @@ let insertar = "InsertarCursos.php";
 let spinner= ` <div class="spinner-border text-success" role="status">
                 <span class="visually-hidden">Loading...</span>
               </div> `
-
 
 
 
@@ -58,7 +59,38 @@ if (nombrePagina == nombreModuloCrear) {
     })
 }
 
-
+if(nombrePagina == nombreModuloListar){
+    formularioEditar.addEventListener('submit', 
+        function(e) {
+        e.preventDefault();//evita que la pagina se recargue
+        
+        let datos = new FormData(formularioEditar);
+    
+        let datosEnviar = {
+                id: datos.get('id'),
+                nombre: datos.get('nombre'),
+                descripcion: datos.get('descripcion'),
+                tiempo: datos.get('tiempo'),
+                usuario: datos.get('usuario'),
+        }
+        console.log(datosEnviar);
+    
+            //url + insertar esto es la url del servicio concatenada
+            fetch( url + actualizar,
+                {
+                    method: 'POST',
+                    body: JSON.stringify(datosEnviar)
+                } 
+            )
+            .then(respuesta=>respuesta.json())
+            .then( (datosrepuesta) => {
+                mensajeActualizar(datosrepuesta)
+                console.log(datosrepuesta);
+                console.log(datosEnviar);
+            })
+            .catch(console.log)
+    })
+}
 
 
 
@@ -83,6 +115,39 @@ function cargarDatos(){
         mostrarDatos(datosrespuesta)
     })
     .catch(console.log)
+}
+
+function mensajeActualizar(datos){
+    if(datos.code == 200){        
+        mensajesSistema.innerHTML = `<div
+                class="alert alert-success alert-dismissible fade show"
+                role="alert"
+            >
+                <button
+                    type="button"
+                    class="btn-close"
+                    data-bs-dismiss="alert"
+                    aria-label="Close"
+                ></button>
+                <strong>Actualizacion exitosa</strong>
+            </div>`;
+
+        setTimeout(cargarDatos, 3000);    
+    }
+    else{
+        mensajesSistema.innerHTML = `<div
+                class="alert alert-danger alert-dismissible fade show"
+                role="alert"
+            >
+                <button
+                    type="button"
+                    class="btn-close"
+                    data-bs-dismiss="alert"
+                    aria-label="Close"
+                ></button>
+                <strong>Error al actualizar</strong>
+            </div>`;
+    }
 }
 
 function mostrarDatos(datos){
@@ -131,6 +196,25 @@ function mostrarDatos(datos){
     document.getElementById("spinnerload").innerHTML = "";
 
 }
+
+
+function editar(datos) {
+    let objeto = JSON.parse(decodeURIComponent(datos));
+    //console.log(objeto);
+    const modalEdicion = new bootstrap.Modal(
+        document.getElementById("modalEditar"));
+        modalEdicion.show();
+        document.getElementById("id").value = objeto.id;
+    document.getElementById("nombre").value = objeto.nombre;
+    document.getElementById("descripcion").value = objeto.descripcion;
+    document.getElementById("tiempo").value = objeto.tiempo;
+    document.getElementById("usuario").value = objeto.usuario;
+    document.getElementById("idCarreras").value = objeto.idCarreras;
+    document.getElementById("idEditar").innerHTML = objeto.id;
+
+
+}
+
 
 function loadspinner(){
 
--- Profesor/scriptProfesor.js
+++ Profesor/scriptProfesor.js
@@ -4,11 +4,13 @@ let formulario = document.getElementById('formulario');
 let nombrePagina = document.title;
 let nombreModuloListar = 'Lista Profesores';
 let nombreModuloCrear = 'Profesor';
+let formularioEditar = document.getElementById('formularioEditar');
 
 
 let url= "https://paginas-web-cr.com/Api/apis/";
 let listar = "ListaProfesores.php";
 let insertar = "InsertarProfesores.php";
+let actualizar = "ActualizarProfesores.php";
 
 
 
@@ -66,7 +68,49 @@ if (nombrePagina == nombreModuloCrear) {
     })
 }
 
-
+if(nombrePagina == nombreModuloListar){
+    formularioEditar.addEventListener('submit', 
+        function(e) {
+        e.preventDefault();//evita que la pagina se recargue
+        
+        let datos = new FormData(formularioEditar);
+    
+        let datosEnviar = {
+                id: datos.get('id'),
+                cedula: datos.get('cedula'),
+                correoelectronico: datos.get('correoelectronico'),
+                telefono: datos.get('telefono'),
+                telefonocelular: datos.get('telefonocelular'),
+                fechanacimiento: datos.get('fechanacimiento'),
+                sexo: datos.get('sexo'),
+                direccion: datos.get('direccion'),
+                nombre: datos.get('nombre'),
+                apellidopaterno: datos.get('apellidopaterno'),
+                apellidomaterno: datos.get('apellidomaterno'),
+                nacionalidad: datos.get('nacionalidad'),
+                idCarreras: datos.get('idCarreras'),
+                usuario: datos.get('usuario'),
+        }
+        console.log(datosEnviar);
+    
+            //url + insertar esto es la url del servicio concatenada
+            fetch( url + actualizar,
+                {
+                    method: 'POST',
+                    body: JSON.stringify(datosEnviar)
+                } 
+            )
+            .then(respuesta=>respuesta.json())
+            .then( (datosrepuesta) => {
+                mensajeActualizar(datosrepuesta)
+                console.log(datosrepuesta);
+                console.log(datosEnviar);
+            })
+            .catch(console.log)
+    
+            
+    })
+}
 
 
 
@@ -81,6 +125,38 @@ function mensajeInsertar(datos){
     }
 }
 
+function mensajeActualizar(datos){
+    if(datos.code == 200){        
+        mensajesSistema.innerHTML = `<div
+                class="alert alert-success alert-dismissible fade show"
+                role="alert"
+            >
+                <button
+                    type="button"
+                    class="btn-close"
+                    data-bs-dismiss="alert"
+                    aria-label="Close"
+                ></button>
+                <strong>Actualizacion exitosa</strong>
+            </div>`;
+
+        setTimeout(cargarDatos, 3000);    
+    }
+    else{
+        mensajesSistema.innerHTML = `<div
+                class="alert alert-danger alert-dismissible fade show"
+                role="alert"
+            >
+                <button
+                    type="button"
+                    class="btn-close"
+                    data-bs-dismiss="alert"
+                    aria-label="Close"
+                ></button>
+                <strong>Error al actualizar</strong>
+            </div>`;
+    }
+}
 
 function cargarDatos(){
     loadspinner();
@@ -149,12 +225,36 @@ function mostrarDatos(datos){
 
 }
 
+
+function editar(datos) {
+    let objeto = JSON.parse(decodeURIComponent(datos));
+    //console.log(objeto);
+    const modalEdicion = new bootstrap.Modal(
+        document.getElementById("modalEditar"));
+        modalEdicion.show();
+    document.getElementById("cedula").value = objeto.cedula;
+    document.getElementById("correoelectronico").value = objeto.correoelectronico;
+    document.getElementById("telefono").value = objeto.telefono;
+    document.getElementById("telefonocelular").value = objeto.telefonocelular;
+    document.getElementById("fechanacimiento").value = objeto.fechanacimiento;
+    document.getElementById("sexo").value = objeto.sexo;
+    document.getElementById("direccion").value = objeto.direccion;
+    document.getElementById("nombre").value = objeto.nombre;
+    document.getElementById("apellidopaterno").value = objeto.apellidopaterno;
+    document.getElementById("apellidomaterno").value = objeto.apellidomaterno;
+    document.getElementById("nacionalidad").value = objeto.nacionalidad;
+    document.getElementById("idCarreras").value = objeto.idCarreras;
+    document.getElementById("usuario").value = objeto.usuario;
+    document.getElementById("id").value = objeto.id;
+    document.getElementById("idEditar").innerHTML = objeto.id;
+
+}
+
 function loadspinner(){
 
     document.getElementById("spinnerload").innerHTML = spinner;
 
 }
-
 
 
 

d3a97dd5e234bccacc192ba56bfe02fa877800c5
Solanito97 <bjsolano97@gmail.com>
2024-05-22 11:13:34 -0600

    add

--- menu.js
+++ menu.js
@@ -7,9 +7,9 @@ let menu = `
                 >
                 <li class="nav-item">
                     <a
-                        href="/Multimedios/Proyecto Personal/index.html"
+                        href="/index.html"
                         class="nav-link active"
-                        data-bs-toggle="tab"
+                        
                         aria-current="page"
                         >Home</a
                     >
@@ -94,7 +94,7 @@ document.getElementById("contenedormenu").innerHTML = menu;
 let pie = `
 <div class="container">
   <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-    <p class="col-md-4 mb-0 text-muted">© 2024 Proyecto Personal, UCR</p>
+    <p class="col-md-4 mb-0 text-muted">© 2024 Proyecto Personal, Byron Solano, UCR</p>
 
     <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
       <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
